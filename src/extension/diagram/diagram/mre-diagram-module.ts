ad762b84a98be69ed64f157d6452680967deed6a
Louis Detweiler <greg.detweiler42@gmail.com>
2025-12-19 10:35:26 -0800

    add node creation tool to diagram module

--- src/extension/diagram/diagram/mre-diagram-module.ts
+++ src/extension/diagram/diagram/mre-diagram-module.ts
@@ -1,25 +1,41 @@
 import {
 	ConsoleLogger,
 	ContainerConfiguration,
+	ContainerManager,
+	Disposable,
+	EnableDefaultToolsAction,
+	InsertIndicator,
+	InsertIndicatorView,
 	LogLevel,
+	NodeCreationTool,
+	NodeCreationToolMouseListener,
 	RoundedCornerNodeView,
 	TYPES,
 	configureActionHandler,
 	configureDefaultModelElements,
+	elementTemplateModule,
 	initializeDiagramContainer,
+	nodeCreationToolModule,
 } from '@eclipse-glsp/client'
 import {
+	Action,
+	bindAsService,
 	configureModelElement,
 	DefaultTypes,
+	DisposableCollection,
 	editLabelFeature,
+	FeatureModule,
+	GhostElement,
 	GLabel,
 	GLabelView,
+	GModelElement,
 	GNode,
 	SetModelAction,
+	TriggerNodeCreationAction,
 	UpdateModelAction,
 } from '@eclipse-glsp/sprotty'
 import 'balloon-css/balloon.min.css'
-import { Container, ContainerModule } from 'inversify'
+import { Container, ContainerModule, injectable } from 'inversify'
 import { ReloadModelActionHandler } from './actions/reload-model-action-handler.js'
 import { ReloadModelAction } from './actions/reload-model-action.js'
 
@@ -41,4 +57,57 @@ const mreDiagramModule = new ContainerModule((bind: any, unbind: any, isBound: a
 
 export function initializeMREDiagramContainer(container: Container, ...containerConfiguration: ContainerConfiguration): Container {
 	return initializeDiagramContainer(container, mreDiagramModule, ...containerConfiguration)
+}
+
+
+export const myNodeCreationToolModule = new FeatureModule(
+	(bind, unbind, isBound, rebind) => {
+		const context = { bind, unbind, isBound, rebind }
+		bindAsService(context, TYPES.IContainerManager, ContainerManager)
+		bindAsService(context, TYPES.ITool, MyNodeCreationTool)
+		configureActionHandler(context, TriggerNodeCreationAction.KIND, MyNodeCreationTool)
+		configureModelElement(context, InsertIndicator.TYPE, InsertIndicator, InsertIndicatorView)
+	},
+	{
+		featureId: nodeCreationToolModule.featureId,
+		requires: elementTemplateModule,
+	},
+)
+
+@injectable()
+export class MyNodeCreationTool extends NodeCreationTool {
+	protected override createNodeCreationListener(ghostElement: GhostElement): Disposable {
+		const toolListener = new MyNodeCreationToolMouseListener(this.triggerAction, this, ghostElement)
+		return new DisposableCollection(toolListener, this.mouseTool.registerListener(toolListener))
+	}
+}
+
+export class MyNodeCreationToolMouseListener extends NodeCreationToolMouseListener {
+	constructor(
+		protected override triggerAction: TriggerNodeCreationAction,
+		protected override tool: NodeCreationTool,
+		protected override ghostElement: GhostElement,
+	) {
+		super(triggerAction, tool, ghostElement)
+		this._dragSensitivity = 100
+	}
+
+	override nonDraggingMouseUp(ctx: GModelElement, event: MouseEvent): Action[] {
+		const result: Action[] = []
+
+		const insert = this.getTrackedInsert(ctx, event)
+		if (insert.valid) {
+			result.push(this.getCreateOperation(ctx, event, insert))
+		}
+		if (this.isContinuousMode(ctx, event)) {
+			// we continue in stamp mode so we keep the ghost but dispose everything else
+			this.disposeAllButGhostElement()
+			return result
+		}
+
+		this.dispose()
+
+		result.push(EnableDefaultToolsAction.create())
+		return result
+	}
 }
